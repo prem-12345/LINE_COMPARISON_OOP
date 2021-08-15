8b3a4818063d9b7a2700b6cc08f61b38d27e2bb7
prem-12345 <premkpatil1997@gmail.com>
2021-08-15 19:57:14 +0530

    WELCOME_TO_LINE_COMPARISON_COMPUTATION_OOP

---
+++ src/com/bridgelabz/linecomparison/LineComparisonMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linecomparison;
+
+public class LineComparisonMain {
+    public static void main(String[] args) {
+
+    //WELCOME TO LINE COMPARISON COMPUTATION OOP
+
+    }
+
+}
